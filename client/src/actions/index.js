6ca96e4f01be29a6a01b864fb1556990aca30af1
Eric <Natelsoneric@gmail.com>
2017-11-05 20:43:00 -0500

    Fixed api url

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Key to access Alphavantage API
 const API_KEY = 'BIYQYMYZ9KIBXS9V';
-const FUNCTIONKEY = 'TIME_SERIES_DAILY';
+const FUNCTIONKEY = 'TIME_SERIES_INTRADAY';
+const INTERVAL = '1min';
 const ROOT_URL = `https://www.alphavantage.co/query?apikey=${API_KEY}`;
 
 
@@ -10,7 +11,7 @@ export const FETCH_DATA = 'FETCH_DATA';
 
 export function fetchData(ticker) {
 
-  const url = `${ROOT_URL}&function=${FUNCTIONKEY}&symbol=${ticker}`
+  const url = `${ROOT_URL}&function=${FUNCTIONKEY}&symbol=${ticker}&interval=${INTERVAL}`;
   const request = axios.get(url);
 
   console.log(request);
