0625c873a3dd326fa33b2d7cd4d6d68c0781e60d
Eric <Natelsoneric@gmail.com>
2017-11-05 20:25:53 -0500

    Added Index action to handle ticker data from API request

---
+++ client/src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+// Key to access Alphavantage API
+const API_KEY = 'BIYQYMYZ9KIBXS9V';
+const FUNCTIONKEY = 'TIME_SERIES_DAILY';
+const ROOT_URL = `https://www.alphavantage.co/query?apikey=${API_KEY}`;
+
+
+export const FETCH_DATA = 'FETCH_DATA';
+
+export function fetchData(ticker) {
+
+  const url = `${ROOT_URL}&function=${FUNCTIONKEY}&symbol=${ticker}`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_DATA,
+    payload: request
+  };
+}
