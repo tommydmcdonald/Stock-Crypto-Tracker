c53aa06426d22011cc61be1752c335b58a43d6fa
Eric <Natelsoneric@gmail.com>
2017-11-05 23:05:35 -0500

    Refactored state to be sotred as an array

--- client/src/reducers/reducer_tracker.js
+++ client/src/reducers/reducer_tracker.js
@@ -1,9 +1,9 @@
 import { FETCH_TRACKER } from '../actions/index';
 
-export default function(state = null, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case FETCH_TRACKER:
-      return action.payload.data;
+      return [action.payload.data, ...state];
   }
   return state;
 }
