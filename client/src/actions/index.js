ea64c38db172ea40d6b243b0bf385decda89038f
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-18 10:01:33 -0500

    Stock and price functionality working

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,8 +7,6 @@ export function fetchTracker(ticker) {
 
   const alpha = require('alphavantage')({ key: 'BIYQYMYZ9KIBXS9V' });
 
-  alpha.data.intraday(ticker);
-
   return {
     type: FETCH_TRACKER,
     payload: alpha.data.intraday(ticker)
