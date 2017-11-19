d7859347f334929d0bbe6bc03cb43fa1e3feedfb
Eric <Natelsoneric@gmail.com>
2017-11-19 15:41:15 -0500

    Plugged in mongoDb as well as Google o auth to authenticate users in a login/logout flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,8 +23,16 @@ passport.use(
     callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile' , profile);
+    User.findOne({ googleId: profile.id}).then((existingUser) => {
+      if (existingUser) {
+        // We already have a record with the given profile Id
+        done(null, existingUser);
+      } else {
+        // We do not have a user id with this profile, make new Id
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      }
+    });
   })
 );
