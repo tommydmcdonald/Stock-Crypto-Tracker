977dc8a9b06acfca076fff5cfae79737325cac93
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-19 00:21:13 -0500

    Refactored index.js in src, including how store was being setup. Also added redux dev tools support

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,23 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import reducers from './reducers';
 import App from './components/App';
 
 // const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-const store = createStore(reducers, null, composedEnhancers)
+const middlewares = [promise];
+const middlewareEnhancer = applyMiddleware(...middlewares);
+
+const storeEnhancers = [middlewareEnhancer];
+
+const composedEnhancer = composeWithDevTools(...storeEnhancers);
+
+const store = createStore(reducers, composedEnhancer)
 
 ReactDOM.render( //most specific routes first in <Switch>
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
      <App />
  </Provider>
 , document.querySelector('#root'));
