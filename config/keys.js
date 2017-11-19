26f4341392d22888e9538eeda570237178bd1096
Eric <Natelsoneric@gmail.com>
2017-11-19 11:35:45 -0500

    Added keys to gitignore file

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '40482564741-olu7i45f8cmdcl9ip21mdss0ve30lujf.apps.googleusercontent.com'
+  googleClientSecret: 'ffyOv8Oi9S8Wsfo_7PyB2t0E'
+};
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express                 = require('express');
-const mongoose                = require('mongoose');
-const passport                = require('passport');
-const bodyParser              = require('bodyParser');
-const LocalStrategy           = require('LocalStrategy');
-const passportLocalMongoose   = require('passportLocalMongoose');
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send('Node.js server');
-});
+passport.use(new GoogleStrategy());
+
+
+//app.get('/', (req, res) => {
+//   res.send('Node.js server');
+//});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
