fb3cd4e31963b85d1316b225f5632466e32e9191
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-20 10:23:48 -0500

    Fixing page update

--- client/src/components/stockcrypto_tracker.js
+++ client/src/components/stockcrypto_tracker.js
@@ -2,15 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export default class StockCryptoTracker extends Component {
-   componentDidMount() {
-      this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
-   }
-
-   componentWillUnmount() {
-      clearInterval(this.interval);
-   }
-
-
+   
    render() {
       return (
          <tr>
