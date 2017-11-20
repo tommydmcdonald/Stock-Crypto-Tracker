6eee48c272b111a438e9acca97ae73cdadbcdece
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-20 18:46:27 -0500

    Added Auth Reducer -Tommy, Taylor, Bryan

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
+
+import authReducer from './authReducer';
 import tickerReducer from './reducer_ticker';
 import dataReducer from './reducer_data';
 
 const rootReducer = combineReducers({
-  tickerList: tickerReducer,
-  dataList: dataReducer
+   auth: authReducer,
+   tickerList: tickerReducer,
+   dataList: dataReducer
 });
 
 export default rootReducer;
