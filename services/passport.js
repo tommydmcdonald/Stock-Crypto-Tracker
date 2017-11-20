ba8ae90db7e4f255605fde47b0ed86f0f6dd1d46
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-20 16:49:46 -0500

    Refactored Google OAuth code from promises to async await

--- services/passport.js
+++ services/passport.js
@@ -6,33 +6,33 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
+   User.findById(id).then(user => {
       done(null, user);
-    });
+   });
 });
 
 passport.use(
-  new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id}).then((existingUser) => {
-      if (existingUser) {
-        // We already have a record with the given profile Id
-        done(null, existingUser);
-      } else {
-        // We do not have a user id with this profile, make new Id
-        new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
+   new GoogleStrategy(
+      {
+         clientID: keys.googleClientID,
+         clientSecret: keys.googleClientSecret,
+         callbackURL: '/auth/google/callback'
+      },
+      async (accessToken, refreshToken, profile, done) => {
+         const existingUser = await User.findOne({ googleId: profile.id });
+
+         if (existingUser) { // We already have a record with the given profile Id
+
+            return done(null, existingUser);
+         }
+         
+         const user = await new User({ googleId: profile.id }).save(); // We do not have a user id with this profile, make new Id
+         done(null, user);
+
       }
-    });
-  })
+   )
 );
