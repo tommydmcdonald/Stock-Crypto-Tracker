46a5df5104f65d001b3a18228d66ec84c7900541
Tommy McDonald <tommydanielmcdonald@gmail.com>
2017-11-30 22:37:27 -0500

    Fixed a bug where Crypto data wasn't being updated automatically in server

--- functions.js
+++ functions.js
@@ -22,16 +22,19 @@ exports.updateTickerData = (intervalMS) => {
          const results = _.map( await axios.all(requests), req => {
             const data = replaceKeys(req.data);
             const metaData = data['Meta Data'];
-
             const type = metaData.hasOwnProperty('2_ Symbol') ? TYPE.STOCK : TYPE.CRYPTO;
-            const name = type == TYPE.STOCK ? metaData['2_ Symbol'] : metaData['2. Digital Currency Code:'];
-
+            const name = type == TYPE.STOCK ? metaData['2_ Symbol'] : metaData['2_ Digital Currency Code'];
+            console.log('name = ', name, ' type = ', type, ' data = ', data);
             return {name, type, data};
          });
 
          _.forEach(results, async (ticker) => {
             const { name, type, data } = ticker;
+            console.log(' _forEach: name = ', name, ' type = ', type, ' data = ', data);
+            const queryTic = await Ticker.findOne( {name, type} );
+            console.log('queryTic = ', queryTic);
             const newTic = await Ticker.findOneAndUpdate( {name, type}, { $set: { 'data.data': data} }, { new: true });
+            console.log('newTic = ', newTic);
          });
       } catch(err) {
          console.log(err);
