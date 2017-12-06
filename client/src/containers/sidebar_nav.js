c262ea312349bdee72fea80532b25a4d7b8f89b2
Eric <Natelsoneric@gmail.com>
2017-12-06 15:16:44 -0500

    Initial container creation

---
+++ client/src/containers/sidebar_nav.js
@@ -0,0 +1,13 @@
+mport React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class SideBarNav extends Component {
+  constructor(props) {
+     super(props);
+  }
+
+
+
+
+}
