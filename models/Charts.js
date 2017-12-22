e00b4db9372dd3e27c417cc310359326e367c7aa
tommydmcdonald <tommydanielmcdonald@gmail.com>
2017-12-21 21:05:37 -0500

    Added Charts model

---
+++ models/Charts.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const TYPE = ['STOCK', 'CRYPTO'];
+
+const chartSchema = new Schema({
+   name: String,
+   type: { type: String, enum: [...TYPE] },
+   data: { frequency: String, data: Schema.Types.Mixed }
+});
+
+mongoose.model('chart', chartSchema);
