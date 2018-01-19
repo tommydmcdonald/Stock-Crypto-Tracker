ba3a13efd74a517573067f84bc49037ce88e6638
tommydmcdonald <tommydanielmcdonald@gmail.com>
2018-01-19 13:11:54 -0500

    Refactored App and Home

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,47 +2,27 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchUser, loadTickerList, loadTickerPrices, selectChart } from '../actions';
-import * as actions from '../actions';
+import { fetchUser } from '../actions';
 
 import '../style/style.css';
 // import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Home from '../components/Home';
+import Home from './Home';
 import Landing from '../components/Landing'
-import ReactInterval from 'react-interval';
 
 class App extends Component {
-   constructor() {
-      super();
-      const refreshRateSeconds = 15;
-      this.timeout = refreshRateSeconds * 1000;
-   }
-
-   async componentDidMount() {
+   componentDidMount() {
       this.props.fetchUser();
-      this.props.loadTickerPrices();
-      await this.props.loadTickerList();
-
-      if (this.props.tickerList[0]) {
-         const { name, type } = this.props.tickerList[0];
-         this.props.selectChart({ name, type });
-      }
    }
 
    render() {
       return (
          <div>
-            <ReactInterval
-               timeout={this.timeout}
-               enabled={true}
-               callback={this.props.loadTickerPrices}
-            />
             <MuiThemeProvider>
             <BrowserRouter>
                <div>
-                  <Route exact path={"/"} component={this.props.auth ? Home : Landing} />
-                  <Route path="/landing" component={Landing} />
+                  <Route exact path='/' component={this.props.auth ? Home : Landing} />
+                  <Route path='/landing' component={Landing} />
                </div>
             </BrowserRouter>
             </MuiThemeProvider>
@@ -56,7 +36,7 @@ function mapStateToProps({ auth, tickerList }) {
 }
 
 function mapDispatchToProps(dispatch) {
-   return bindActionCreators({ fetchUser, loadTickerList, loadTickerPrices, selectChart }, dispatch);
+   return bindActionCreators({ fetchUser }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App); //guessint no mapdispatchtoprops cause no need to bind aciton creators causing using redux-thunk?
---
+++ client/src/containers/Home.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { loadTickerList, loadTickerPrices, selectChart } from '../actions';
+
+import Header from './Header';
+import Chart from './Chart';
+import SearchBar from './SearchBar';
+import SideNav from '../components/SideNav';
+import { Row, Col } from 'react-materialize';
+import ReactInterval from 'react-interval';
+
+class Home extends Component {
+   constructor() {
+      super();
+      const refreshRateSeconds = 15;
+      this.timeout = refreshRateSeconds * 1000;
+   }
+
+   async componentDidMount() {
+      this.props.loadTickerPrices();
+      await this.props.loadTickerList();
+
+      if (this.props.tickerList[0]) {
+         const { name, type } = this.props.tickerList[0];
+         this.props.selectChart({ name, type });
+      }
+   }
+
+   render() {
+      return (
+         <div className='mainApp'>
+            <ReactInterval
+               timeout={this.timeout}
+               enabled={true}
+               callback={this.props.loadTickerPrices}
+            />
+
+            <Header />
+            <Row> <Col s={9} offset="s3"> <SearchBar /> </Col></Row>
+            <Row>
+              <Col s={4}><SideNav s={12}/></Col>
+              <Col s={8}><Chart /></Col>
+            </Row>
+         </div>
+       );
+   }
+}
+
+function mapStateToProps({tickerList, priceList, selectedChart}) {
+   return { tickerList, priceList, selectedChart }
+}
+
+function mapDispatchToProps(dispatch) {
+   return bindActionCreators({ loadTickerList, loadTickerPrices, selectChart }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
