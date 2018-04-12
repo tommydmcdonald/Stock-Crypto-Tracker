ab98e7e53a6f30c754acbffaac630f7bc1a1879f
tommydmcdonald <tommydanielmcdonald@gmail.com>
2018-04-12 18:50:08 -0400

    Fixed searchbar not clearing when submitting

--- client/src/containers/SearchBar.js
+++ client/src/containers/SearchBar.js
@@ -37,6 +37,7 @@ class SearchBar extends Component {
 
       if ( newTicker.name !== '' && !_.some(this.props.tickerList, newTicker) ) { //only add if not empty string and ticker doesn't exist in redux tickerList
          this.props.addTicker(newTicker, this.props.tickerList.length);
+         this.setState({ticker: ''});
       } //
    }
 
